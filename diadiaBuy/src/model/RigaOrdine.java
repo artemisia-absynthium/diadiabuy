b7e99a8e5461818b268bec43b93f2dc7daada35f
Cristina De Rito <cristina.derito@outlook.com>
2010-04-08 21:50:33 +0000

    Initial commit: Model classes.

---
+++ diadiaBuy/src/model/RigaOrdine.java
@@ -0,0 +1,32 @@
+package model;
+
+public class RigaOrdine {
+	private int id;
+	private int quantita;
+	private Prodotto prodotto;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+
+	public Prodotto getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(Prodotto prodotto) {
+		this.prodotto = prodotto;
+	}
+
+}
